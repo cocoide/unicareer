f917a6155e109bb2051b43169b878dc02d3a02e2
{cocoide} <{cocoide.com@gmail.com}>
2023-04-22 03:14:41 +0900

    delete needless files

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,18 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./features/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: "#43d487",
+        darkbase: "#268951",
+        lightbase: "#87eac5",
+        neutral: "#cbe4d4",
       },
     },
   },
   plugins: [],
-}
+};
