e23181f765cb7a6acdf68040cc5865f2361e7279
{cocoide} <{cocoide.com@gmail.com}>
2023-04-23 04:12:28 +0900

    update header

--- frontend/components/Layouts/Header.tsx
+++ frontend/components/Layouts/Header.tsx
@@ -1,12 +1,18 @@
+import { Bars3BottomLeftIcon, BellIcon } from '@heroicons/react/24/outline'
+import Image from 'next/image'
+
 const Header = () => {
     return (
-        <div className="flex flex-col w-full items-center">
-            <div className="flex flex-row justify-between items-center py-2 px-10 w-full border-b border-gray-200">
+        <div className="flex flex-row justify-between items-center py-2 px-5 md:px-10 w-full border-b border-gray-200">
+            <Bars3BottomLeftIcon className='h-7 w-7 text-gray-400 md:hidden' />
                 {/* Service Name*/}
                 <div className="text-primary font-bold text-2xl"
                 >Unicareer</div>
+            <div className="flex items-center justify-center space-x-5">
                 {/* Avatar Button*/}
-                <div className="bg-gray-200 h-10 w-10 rounded-full shadow-sm" />
+                <BellIcon className='h-7 w-7 text-gray-400' />
+                <Image src="/avater.jpeg" alt="avater" width={100} height={100}
+                    className="bg-gray-200 h-10 w-10 rounded-full hidden md:flex" />
             </div>
         </div>
     )
