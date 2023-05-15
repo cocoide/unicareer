619631cc295bf7f1e5f37cafb34673f09720e595
{cocoide} <{cocoide.com@gmail.com}>
2023-05-15 20:29:41 +0900

    feat: update navigation for UX and add new pages

--- frontend/components/Layouts/Mobile/Navigation.tsx
+++ frontend/components/Layouts/Mobile/Navigation.tsx
@@ -1,15 +1,27 @@
+"use client"
+import { clsx } from '@/utils/clsx'
 import { ChatBubbleOvalLeftEllipsisIcon, DocumentPlusIcon, HomeIcon, PlusCircleIcon, UserCircleIcon } from '@heroicons/react/24/outline'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
+const icon_style = 'h-6 w-6'
+const navigations = [
+  { path: "/", icon: <HomeIcon className={icon_style} /> },
+  { path: "/es", icon: <DocumentPlusIcon className={icon_style} /> },
+  { path: "/post", icon: <PlusCircleIcon className={icon_style} /> },
+  { path: "/chat", icon: <ChatBubbleOvalLeftEllipsisIcon className={icon_style} /> },
+  { path: "/mypage", icon: <UserCircleIcon className={icon_style} /> },
+]
 const Navigation = () => {
+  const currentPath = usePathname()
   return (
     <div className="flex items-center justify-center  h-12 w-full 
     fixed bottom-0 py-1 space-x-8 md:hidden text-gray-500 border-t border-gray-200 shadow-md bg-white">
-        <Link href="/" className='bg-neutral p-2 rounded-full' ><HomeIcon className='h-6 w-6 text-white'/> </Link>
-      <Link href="/" className='bg-slate-50 p-2 rounded-full'><DocumentPlusIcon className='h-6 w-6' /> </Link>
-        <Link href="/"  className='bg-slate-50 p-2 rounded-full'><PlusCircleIcon className='h-6 w-6'/> </Link>
-        <Link href="/"  className='bg-slate-50 p-2 rounded-full'><ChatBubbleOvalLeftEllipsisIcon className='h-6 w-6'/> </Link>
-      <Link href="/mypage" className='bg-slate-50 p-2 rounded-full'><UserCircleIcon className='h-6 w-6' /> </Link>
+      {navigations.map((navi, i) => (
+        <Link key={i} href={navi.path} className={clsx('p-2 rounded-full duration-100',
+          currentPath === navi.path ? "bg-neutral text-white" : "bg-slate-50")}
+        >{navi.icon}</Link>
+      ))}
 </div>
   )
 }
